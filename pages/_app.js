c9915c704d6102bf2464c53357a0efa91f1de3bc
Sodiq Sanusi <ademilddiq@gmail.com>
2023-01-18 10:07:29 +0100

    Tried using the new Next font optimizer (Google Fonts)
    Will remove the google fonts now and try the same for local fonts. Just want to get more familiar with using Next.js 13 features

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { Ubuntu } from '@next/font/google'
+
+const ubuntu = Ubuntu({
+  weight: ['400', '500', '700'],
+  subsets: ['latin']
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={ubuntu.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
