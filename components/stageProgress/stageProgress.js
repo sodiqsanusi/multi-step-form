a6276a7067cc15e8862febb72fe29c405abf5cca
Sodiq Sanusi <ademilddiq@gmail.com>
2023-01-18 14:22:01 +0100

    Atomic commits to track progress better and retrace steps quicker

--- components/stageProgress/stageProgress.js
+++ components/stageProgress/stageProgress.js
@@ -3,7 +3,36 @@ import styles from './stageProgress.module.css';
 const StageProgress = () => {
   return ( 
     <section className={styles.container}>
-      stageprogress
+      <ul className={styles.progressList}>
+        <li>
+          <div className={`${styles.progressListNum}`}>1</div>
+          <div className={styles.progressListExp}>
+            <p>step 1</p>
+            <p>your info</p>
+          </div>
+        </li>
+        <li>
+          <div className={`${styles.progressListNum}`}>2</div>
+          <div className={styles.progressListExp}>
+            <p>step 2</p>
+            <p>select plan</p>
+          </div>
+        </li>
+        <li>
+          <div className={`${styles.progressListNum}`}>3</div>
+          <div className={styles.progressListExp}>
+            <p>step 3</p>
+            <p>add-ons</p>
+          </div>
+        </li>
+        <li>
+          <div className={`${styles.progressListNum}`}>4</div>
+          <div className={styles.progressListExp}>
+            <p>step 4</p>
+            <p>summary</p>
+          </div>
+        </li>
+      </ul>
     </section>
   );
 }
