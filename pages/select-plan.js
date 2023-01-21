ff9a1f60a3c86aba5a5c5be21a12c2c710b1b44f
Sodiq Sanusi <ademilddiq@gmail.com>
2023-01-21 07:51:59 +0100

    Atomic commits to track progress better and retrace steps quicker

--- pages/select-plan.js
+++ pages/select-plan.js
@@ -1,6 +1,13 @@
-import Head from 'next/head'
-import { useContext, useEffect } from 'react'
-import { globalContext } from '../globalContext'
+import Head from 'next/head';
+import { useContext, useEffect } from 'react';
+import { globalContext } from '../globalContext';
+import Container from '../components/container/container';
+import styles from '../styles/select-plan.module.css';
+import Image from 'next/image';
+import arcadeImage from '../public/images/icon-arcade.svg';
+import advancedImage from '../public/images/icon-advanced.svg';
+import proImage from '../public/images/icon-pro.svg';
+
 
 const SelectPlan = () => {
 
@@ -14,9 +21,45 @@ const SelectPlan = () => {
       <Head>
         <title>Select your preferred plan</title>
       </Head>
-      <main>
-        Select your plan
-      </main>
+      <Container>
+        <h1>Select your plan</h1>
+        <h2>You have the option of monthly or yearly billing.</h2>
+        <section className={styles.layout}>
+          <button>
+            <div><Image src={arcadeImage} aria-hidden='true' alt='Pick the arcade plan'/></div>
+            <div>
+              <h3>Arcade</h3>
+              <p>$9/mo</p>
+              <p>2 months free</p>
+            </div>
+          </button>
+          <button>
+            <div><Image src={advancedImage} aria-hidden='true' alt='Pick the advanced plan'/></div>
+            <div>
+              <h3>Advanced</h3>
+              <p>$12/mo</p>
+              <p>2 months free</p>
+            </div>
+          </button>
+          <button>
+            <div><Image src={proImage} aria-hidden='true' alt='Pick the pro plan'/></div>
+            <div>
+              <h3>Pro</h3>
+              <p>$15/mo</p>
+              <p>2 months free</p>
+            </div>
+          </button>
+        </section>
+        <div>
+          <div>
+            <p>Monthly</p>
+            <button>
+              <span></span>
+            </button>
+            <p>Yearly</p>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
