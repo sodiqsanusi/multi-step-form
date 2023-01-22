21e0f02f731780f85df306b52eda265464524175
Sodiq Sanusi <ademilddiq@gmail.com>
2023-01-22 08:05:23 +0100

    Atomic commits to track progress better and retrace steps quicker

--- pages/addons.js
+++ pages/addons.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useContext, useEffect } from 'react'
 import { globalContext } from '../globalContext'
+import Container from "../components/container/container";
 
 
 const AddOns = () => {
@@ -15,9 +16,36 @@ const AddOns = () => {
       <Head>
         <title>Choose add-ons you want</title>
       </Head>
-      <main>
-        Add-ons!
-      </main>
+      <Container>
+        <h1>Pick add-ons</h1>
+        <h2>Add-ons help enhance your gaming experience</h2>
+        <section>
+          <label htmlFor="onlineService">
+            <input type="checkbox" name="onlineService" id="onlineService"/>
+            <div>
+              <h3>Online service</h3>
+              <p>Access to multiplayer games</p>
+            </div>
+            <p>+$1/mo</p>
+          </label>
+          <label htmlFor="largerStorage">
+            <input type="checkbox" name="largerStorage" id="largerStorage"/>
+            <div>
+              <h3>Larger storage</h3>
+              <p>Extra 1TB of cloud save</p>
+            </div>
+            <p>+$2/mo</p>
+          </label>
+          <label htmlFor="customProfile">
+            <input type="checkbox" name="customProfile" id="customProfile"/>
+            <div>
+              <h3>Customizable profile</h3>
+              <p>Custom theme on your profile</p>
+            </div>
+            <p>+$2/mo</p>
+          </label>
+        </section>
+      </Container>
     </>
     
   );
