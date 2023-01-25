54e69202a8f21f29f1986126113976712c430a46
Sodiq Sanusi <ademilddiq@gmail.com>
2023-01-25 07:54:13 +0100

    Atomic commits to track progress better and retrace steps quicker

--- globalContext.js
+++ globalContext.js
@@ -5,9 +5,12 @@ export const globalContext = createContext();
 export const GlobalProvider = ({children}) => {
   let [stage, setStage] = useState(1);
   let [renewal, setRenewal] = useState('month');
+  let [plan, setPlan] = useState('arcade');
 
   return(
-    <globalContext.Provider value={{stage, setStage, renewal, setRenewal}}>
+    <globalContext.Provider
+     value={{stage, setStage, renewal, setRenewal, plan, setPlan}}
+    >
       {children}
     </globalContext.Provider>
   )
